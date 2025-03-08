ea4fe8c5c02eba9162555cbff0553480353e7819
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-03-08 17:08:24 +0530

    Section 6 - Done

---
+++ src/test/java/dummyFeatureFolder/MyStepdefs.java
@@ -0,0 +1,50 @@
+package dummyFeatureFolder;
+
+import io.cucumber.java.en.*;
+
+public class MyStepdefs {
+
+    @Given("I am on the fake Register page")
+    public void i_am_on_the_fake_register_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I enter my details")
+    public void i_enter_my_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I enter my lastname")
+    public void i_enter_my_lastname() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I enter my username")
+    public void i_enter_my_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I enter my password")
+    public void i_enter_my_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click on the Adult checkbox")
+    public void i_click_on_the_adult_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be registered and navigated to the Login page")
+    public void i_should_be_registered_and_navigated_to_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ src/test/java/dummyFeatureFolder/dummy.feature
@@ -0,0 +1,15 @@
+Feature: Dummy Feature
+  As a dummy User visiting a fake website
+  If I enter my details in the Register User Form
+  My Account should be created on the website
+
+  Scenario: Dummy Register
+    Given I am on the fake Register page
+    When I enter my details
+    And I enter my lastname
+    And I enter my username
+    And I enter my password
+    And I click on the Adult checkbox
+    Then I should be registered and navigated to the Login page
+
+
